e1c76bc09c923d96a61d658dcd5a168362d4ee00
Abir M <102193884+abirmahmood6@users.noreply.github.com>
2025-12-10 05:25:44 -0500

    navbar complete

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import { PlusIcon } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <header className="bg-gray-800 text-white px-6 py-4">
+      <div className="mx-auto max-w-6xl ">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
+            <Link to={"/"}>CreatorVerse</Link>
+          </h1>
+          <div className="flex items-center gap-4">
+            <Link to={"/add"} className="btn btn-primary">
+              <PlusIcon className="size-5" />
+              <span> Add Creator</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+    </header>
+  );
+};
+
+export default Navbar;
